718a62b04b601b94fc4114d036abcc17725c97b6
Andry Pebrianto <andrypeb227@gmail.com>
2022-01-20 21:35:37 +0700

    Navbar Design

---
+++ src/components/Layout/Navbar/Cart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function Cart() {
+  return (
+    <div style={{ color: "#4A4A4B" }} className="fs-5">
+      <FaShoppingCart /> <sup>3</sup>
+    </div>
+  );
+}
