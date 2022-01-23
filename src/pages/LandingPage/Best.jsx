ad690c7812369baa6930bf21fc28cf28c23cb6cd
Andry Pebrianto <andrypeb227@gmail.com>
2022-01-24 00:50:22 +0700

    Create produk unggulan component for landing page

---
+++ src/pages/LandingPage/Best.jsx
@@ -0,0 +1,231 @@
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import blue from "./blue.png";
+
+export default function Best() {
+  return (
+    <section id="projects" class="mb-4 semi semi-light">
+      <div class="container-lg py-5">
+        <h2 class="fw-bold text-center mb-3 mt-2">Produk Unggulan</h2>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
+          <div class="col my-2">
+            <div class="card shadow-sm h-100">
+              <div class="card-img">
+                <img
+                  class="img-fluid"
+                  src={blue}
+                  alt="Caesar Cipher Web App Thumbnail"
+                />
+              </div>
+              <div class="card-body">
+                <p class="fs-5 fw-bold card-title">Sate Ayam</p>
+                <p class="card-text mb-2">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Voluptate quaerat mollitia id veniam ex delectus expedita
+                  dolorum doloribus tempore debitis?
+                </p>
+              </div>
+              <div class="card-footer">
+                <div class="d-flex justify-content-between">
+                  <a href="#projects" class="btn btn-primary me-auto">
+                    Detail
+                  </a>
+                  <div>
+                    <a
+                      href="https://caesarcipher1.netlify.app/"
+                      class="btn btn-success"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaShoppingCart /> <sup>+</sup>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col my-2">
+            <div class="card shadow-sm h-100">
+              <div class="card-img">
+                <img
+                  class="img-fluid"
+                  src={blue}
+                  alt="Caesar Cipher Web App Thumbnail"
+                />
+              </div>
+              <div class="card-body">
+                <p class="fs-5 fw-bold card-title">Sate Ayam</p>
+                <p class="card-text mb-2">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Voluptate quaerat mollitia id veniam ex delectus expedita
+                  dolorum doloribus tempore debitis?
+                </p>
+              </div>
+              <div class="card-footer">
+                <div class="d-flex justify-content-between">
+                  <a href="#projects" class="btn btn-primary me-auto">
+                    Detail
+                  </a>
+                  <div>
+                    <a
+                      href="https://caesarcipher1.netlify.app/"
+                      class="btn btn-success"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaShoppingCart /> <sup>+</sup>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col my-2">
+            <div class="card shadow-sm h-100">
+              <div class="card-img">
+                <img
+                  class="img-fluid"
+                  src={blue}
+                  alt="Caesar Cipher Web App Thumbnail"
+                />
+              </div>
+              <div class="card-body">
+                <p class="fs-5 fw-bold card-title">Sate Ayam</p>
+                <p class="card-text mb-2">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Voluptate quaerat mollitia id veniam ex delectus expedita
+                  dolorum doloribus tempore debitis?
+                </p>
+              </div>
+              <div class="card-footer">
+                <div class="d-flex justify-content-between">
+                  <a href="#projects" class="btn btn-primary me-auto">
+                    Detail
+                  </a>
+                  <div>
+                    <a
+                      href="https://caesarcipher1.netlify.app/"
+                      class="btn btn-success"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaShoppingCart /> <sup>+</sup>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col my-2">
+            <div class="card shadow-sm h-100">
+              <div class="card-img">
+                <img
+                  class="img-fluid"
+                  src={blue}
+                  alt="Caesar Cipher Web App Thumbnail"
+                />
+              </div>
+              <div class="card-body">
+                <p class="fs-5 fw-bold card-title">Sate Ayam</p>
+                <p class="card-text mb-2">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Voluptate quaerat mollitia id veniam ex delectus expedita
+                  dolorum doloribus tempore debitis?
+                </p>
+              </div>
+              <div class="card-footer">
+                <div class="d-flex justify-content-between">
+                  <a href="#projects" class="btn btn-primary me-auto">
+                    Detail
+                  </a>
+                  <div>
+                    <a
+                      href="https://caesarcipher1.netlify.app/"
+                      class="btn btn-success"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaShoppingCart /> <sup>+</sup>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col my-2">
+            <div class="card shadow-sm h-100">
+              <div class="card-img">
+                <img
+                  class="img-fluid"
+                  src={blue}
+                  alt="Caesar Cipher Web App Thumbnail"
+                />
+              </div>
+              <div class="card-body">
+                <p class="fs-5 fw-bold card-title">Sate Ayam</p>
+                <p class="card-text mb-2">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Voluptate quaerat mollitia id veniam ex delectus expedita
+                  dolorum doloribus tempore debitis?
+                </p>
+              </div>
+              <div class="card-footer">
+                <div class="d-flex justify-content-between">
+                  <a href="#projects" class="btn btn-primary me-auto">
+                    Detail
+                  </a>
+                  <div>
+                    <a
+                      href="https://caesarcipher1.netlify.app/"
+                      class="btn btn-success"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaShoppingCart /> <sup>+</sup>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col my-2">
+            <div class="card shadow-sm h-100">
+              <div class="card-img">
+                <img
+                  class="img-fluid"
+                  src={blue}
+                  alt="Caesar Cipher Web App Thumbnail"
+                />
+              </div>
+              <div class="card-body">
+                <p class="fs-5 fw-bold card-title">Sate Ayam</p>
+                <p class="card-text mb-2">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Voluptate quaerat mollitia id veniam ex delectus expedita
+                  dolorum doloribus tempore debitis?
+                </p>
+              </div>
+              <div class="card-footer">
+                <div class="d-flex justify-content-between">
+                  <a href="#projects" class="btn btn-primary me-auto">
+                    Detail
+                  </a>
+                  <div>
+                    <a
+                      href="https://caesarcipher1.netlify.app/"
+                      class="btn btn-success"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaShoppingCart /> <sup>+</sup>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
